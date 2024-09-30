08d2b0ea18d0fd3dcf59962fb5bced21727f5b32
Hernandes <Hernandeshass910@gmail.com>
2024-09-30 07:19:37 -0300

    feat: adicionando menu alterar na tab autor
    - classes alterar na entidade Autor
    - reformulando lógica tabs página alterar para permanecer na aba ao enviar um formulário

--- pages/alterar.php
+++ pages/alterar.php
@@ -29,7 +29,7 @@
                     <fieldset>
                         <legend>Alterando</legend>
                         <p>Digite o nome do livro que deseja alterar:</p>
-                        <input type="text" placeholder="Nome do livro" name="txtnome">
+                        <input class="input-autoria" type="text" placeholder="Nome do livro" name="txtnome">
                         <br>
                         <input class="botao" type="submit" value="Consultar" name="btnconsultar">
                     </fieldset>
@@ -135,7 +135,7 @@
                     <fieldset>
                         <legend>Alterando</legend>
                         <p>Digite o nome do autor que deseja alterar:</p>
-                        <input type="text" placeholder="Nome do autor" name="txtnome">
+                        <input class="input-autoria" type="text" placeholder="Nome do autor" name="txtnome">
                         <br>
                         <input class="botao" type="submit" value="Consultar" name="btnconsultar">
                     </fieldset>
@@ -229,22 +229,108 @@
             </div>
 
         <div class="tab-content <?php echo ($activeTab === 'tab3') ? 'active' : ''; ?>" id="tab3">
-            <!-- Conteúdo do formulário da aba "Autoria" -->
-            <form name="autoria" method="POST" action="">
-                <input type="hidden" name="form_type" value="autoria">
-                <fieldset id="a">
-                    <legend><b>Dados da Autoria</b></legend>
-                    <p>Código do Autor: <input name="autor" type="number" placeholder="Código do Autor" required></p>
-                    <p>Código do Livro: <input name="livro" type="number" placeholder="Código do Livro" required></p>
-                    <p>Data de Lançamento: <input name="lancamento" type="date" required></p>
-                    <p>Editora: <input name="editora" type="text" placeholder="Editora" required></p>
-                </fieldset>
-                <fieldset id="b">
-                    <legend><b>Opções:</b></legend>
-                    <input class="botao" name="btenviar_autoria" type="submit" value="Cadastrar">
-                    <input name="limpar" type="reset" value="Limpar">
-                </fieldset>
-            </form>
+
+        <div class="consultar">
+                <form method="POST" action="">
+                    <input type="hidden" name="activeTab" value="tab3">
+                    <input type="hidden" name="form_name" value="alterar1">
+                    <fieldset>
+                        <legend>Alterando</legend>
+                        <p>Digite os dados da autoria que deseja alterar:</p>
+                        <input class="input-autoria" type="text" placeholder="Código autor" name="txtcodautor">
+                        <input class="input-autoria" type="text" placeholder="Código livro" name="txtcodlivro">
+                        <br>
+                        <input class="botao" type="submit" value="Consultar" name="btnconsultar">
+                    </fieldset>
+                </form>
+            </div>
+
+            <?php
+                if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['form_name'] === 'alterar1') {
+                    extract($_POST, EXTR_OVERWRITE);
+                    if(isset($btnconsultar)) {
+                        $txtnome = $_POST['txtnome'];
+                        include_once '../models/Autor.php';
+                        $a = new Autor();
+                        $a->setNome($txtnome);
+                        $autor = $a->alterar();
+                    }
+
+                    if ($autor) {
+
+                        foreach($autor as $autor_mostrar){
+
+                        ?>
+
+                        <div class="alterar">
+
+                        <form method="POST" action="">
+                            <input type="hidden" name="activeTab" value="tab3">
+                            <input type="hidden" name="form_name" value="alterar2">
+                            <fieldset id="a">
+                                <legend><b>Dados do Autor</b></legend>
+                                <p>Código Autor: <input hidden name="codigo" type="text" size="40" maxlength="100" value="<?php echo $autor_mostrar[0] ?>" required></p>
+                                <p>Nome: <input name="nome" type="text" size="40" maxlength="100" placeholder="Título do Livro" value="<?php echo $autor_mostrar[1] ?>" required></p>
+                                <p>Sobrenome: <input name="sobrenome" type="text" size="40" maxlength="40" placeholder="Categoria" value="<?php echo $autor_mostrar[2] ?>" required></p>
+                                <p>Email: <input name="email" type="text" size="40" maxlength="20" placeholder="ISBN" value="<?php echo $autor_mostrar[3] ?>" required></p>
+                                <p>Nascimento: <input name="nasc" type="text" size="40" maxlength="20" placeholder="Idioma" value="<?php echo $autor_mostrar[4] ?>" required></p>
+                            </fieldset>
+                            <fieldset id="b">
+                                <legend><b>Opções:</b></legend>
+                                <input class="botao" name="btnalterar" type="submit" value="Alterar"> &nbsp;&nbsp;
+                            </fieldset>
+                        </form>
+
+                        </div>
+
+                        <?php
+                            }
+                        ?>
+
+                        <?php
+                    } else {
+                        echo "<div class='message'><b><p>Nome do autor não encontrado.</p></b></div>";
+                    }
+                }
+            ?>
+
+            <?php
+                if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['form_name'] === 'alterar2') {
+                    extract($_POST, EXTR_OVERWRITE);
+                    if (isset($btnalterar)) {
+                        include_once '../models/Autor.php';
+                        $a = new Autor();
+
+                        $codigo = $_POST['codigo'];
+                        $nome = $_POST['nome'];
+                        $sobrenome = $_POST['sobrenome'];
+                        $email = $_POST['email'];
+                        $nasc = $_POST['nasc'];
+                        
+                        $a->setCodigo($codigo);
+                        $a->setNome($nome);
+                        $a->setSobrenome($sobrenome);
+                        $a->setEmail($email);
+                        $a->setNasc($nasc);
+                        
+                        $resultado = $a->alterar2();
+                        
+                        
+                        if ($resultado) {
+                            echo $resultado;
+                        } else {
+                            echo $resultado;
+                        }
+                    }
+                }
+
+            ?>
+            
+            <div class="button-container">
+                <a href="../index.html" class="botao">Voltar</a>
+            </div>
+            </div>
+
         </div>
     </div>
 

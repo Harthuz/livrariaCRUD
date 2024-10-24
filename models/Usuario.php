fd0eb95ed25184bd87238e0ef6695c509758338a
Hernandes <Hernandeshass910@gmail.com>
2024-10-24 14:36:37 -0300

    style: adicionando estilo tela login

--- models/Usuario.php
+++ models/Usuario.php
@@ -2,6 +2,18 @@
 include_once 'conectar.php';
 
 Class usuario{
+    private $usuario;
+    private $senha;
+
+    get get$usuario => this.$usuario;
+
+    set set$usuario( $usuario) => this.$usuario = $usuario;
+
+    get get$senha => this.$senha;
+
+    set set$senha( $senha) => this.$senha = $senha;
+
     
+
 }
 ?>
